3f853ff7d16ae0237e630328a9162306f56a6723
diegoslompo <diegoslompo@gmail.com>
2019-02-16 20:32:17 -0200

    Fix cards actions, styles

--- components/AddDeckComp.js
+++ components/AddDeckComp.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { Button } from './Button';
+import { fetchData, storeData } from '../utils/api';
 import { colors } from '../utils/helpers';
 import PropTypes from 'prop-types';
 
@@ -9,6 +10,67 @@ class AddDeckComp extends Component {
   state = {
     title: '',
     titleError: false,
+  }
+
+  onPress = (ev) => {
+    const { title } = this.state;
+    const id = this.props.navigation.getParam('deckId');
+
+    if (title.length) {
+      fetchData(data => {
+        let newData = [];
+        let newDeck = null;
+        let key = id || '1';
+
+        if (data) {
+          prevData = JSON.parse(data);
+          if (id) {
+            newData = prevData.map(item => {
+              if (item.id === id) {
+                item.title = title;
+              }
+              return item;
+            });
+          } else {
+            key = (newData.length + 1).toString();
+
+            newDeck = {
+              key,
+              id: `deck-${key}`,
+              title,
+              cardsCount: 0,
+              cards: [],
+            }
+
+            newData = prevData.concat(newDeck);
+          }
+        }
+
+        storeData(newData, data => {
+          this.props.navigation.navigate('Deck', {
+            deckId: id || newDeck.id,
+            deckTitle: title,
+            cardsCount: 0,
+          });
+        });
+      });
+    } else {
+      this.setState({
+        titleError: true,
+      });
+    }
+  }
+
+  componentDidMount() {
+    const id = this.props.navigation.getParam('deckId');
+    const title = this.props.navigation.getParam('deckTitle');
+    
+    if (id && title) {
+      this.setState({
+        id,
+        title,
+      });
+    }
   }
 
   render() {
@@ -34,7 +96,21 @@ class AddDeckComp extends Component {
   }
 }
 
-
+const styles = StyleSheet.create({
+  formControl: {
+    padding: 20,
+  },
+  formLabel: {
+    fontSize: 16,
+  },
+  formInput: {
+    height: 40,
+  },
+  formError: {
+    color: colors.red,
+    fontSize: 14,
+  }
+});
 
 AddDeckComp.propTypes = {
   navigation: PropTypes.object.isRequired,
